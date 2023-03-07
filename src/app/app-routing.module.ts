1dd2d508564a7701612003e65fbdeba65c1156b4
BorosFather <boros.zoli024@gmail.com>
2023-03-07 17:19:44 +0100

    kész

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { TauloComponent } from './taulo/taulo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "tanulo", component:TauloComponent},
+  {path: "about", component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
