9ec2b0a5eeda01047cb5ed381b579165233201ee
BorosFather <boros.zoli024@gmail.com>
2023-03-07 16:13:38 +0100

    api servide megírva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiComponent } from './sherd/api/api.component';
+import { HttpClientModule } from '@angular/common/http';
 import { TauloComponent } from './taulo/taulo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApiComponent,
     TauloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
